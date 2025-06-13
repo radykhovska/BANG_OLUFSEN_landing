5e57353230a1e45c801102d3a6469e633195087a
radykhovska <zagnibeda@i.ua>
2025-06-13 14:59:47 +0300

    make build

---
+++ index.f3c10c9c.js
@@ -0,0 +1,2 @@
+var t=window.innerHeight;window.addEventListener("scroll",function(){var e=document.getElementById("top-bar");window.scrollY>t?e.classList.add("sticky"):e.classList.remove("sticky")});
+//# sourceMappingURL=index.f3c10c9c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const headerHeight = window.innerHeight;
+window.addEventListener("scroll", ()=>{
+    const topBar = document.getElementById("top-bar");
+    // scrollY - scrolled from top
+    if (window.scrollY > headerHeight) topBar.classList.add("sticky");
+    else topBar.classList.remove("sticky");
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
