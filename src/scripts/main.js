e39f4e0e7e7d69af33d04bcd84f509417524db38
radykhovska <zagnibeda@i.ua>
2025-06-13 14:59:04 +0300

    add landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const headerHeight = window.innerHeight;
+
+window.addEventListener('scroll', () => {
+  const topBar = document.getElementById('top-bar');
+
+  // scrollY - scrolled from top
+  if (window.scrollY > headerHeight) {
+    topBar.classList.add('sticky');
+  } else {
+    topBar.classList.remove('sticky');
+  }
+});
